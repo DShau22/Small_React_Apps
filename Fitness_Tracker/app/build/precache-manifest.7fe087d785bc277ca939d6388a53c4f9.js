57065d385beadf9239f72396a0afad9cda6d58a5
David Shau <davidshau22@berkeley.edu>
2020-02-04 22:10:38 -0800

    built the react app

---
+++ Fitness_Tracker/app/build/precache-manifest.7fe087d785bc277ca939d6388a53c4f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89397df34736d39206e46be0a26ffb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0dab13065b0f20b98fd",
+    "url": "/static/css/2.bfcdf27e.chunk.css"
+  },
+  {
+    "revision": "6ccc986129514df6f897",
+    "url": "/static/css/main.302284e2.chunk.css"
+  },
+  {
+    "revision": "c0dab13065b0f20b98fd",
+    "url": "/static/js/2.a5d3c5bd.chunk.js"
+  },
+  {
+    "revision": "6ccc986129514df6f897",
+    "url": "/static/js/main.f06db4b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- Fitness_Tracker/app/src/utils/userInfo.js
+++ Fitness_Tracker/app/src/utils/userInfo.js
@@ -2,63 +2,63 @@ const getBestsURL = "http://localhost:8080/getBests"
 const getProfileURL = "http://localhost:8080/getProfilePic"
 const getUsernameURL = "http://localhost:8080/getUsername"
 
-  /**
-   * gets the bests of a user given their id
-   */
-  async function getBests(id) {
-    var reqBody = { id }
-    var res = await fetch(getBestsURL, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(reqBody)
-    })
-    var bestsJson = await res.json()
-    if (bestsJson.success) {
-      return bestsJson.bests
-    }
-    alert("could not get bests for id: ", id)
+/**
+ * gets the bests of a user given their id
+ */
+export async function getBests(id) {
+  var reqBody = { id }
+  var res = await fetch(getBestsURL, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reqBody)
+  })
+  var bestsJson = await res.json()
+  if (bestsJson.success) {
+    return bestsJson.bests
   }
-
-  /**
-   * gets the profile pic url of the user 
-   * given their user id in Mongo
-   */
-  async function getProfile(id) {
-    var reqBody = { id }
-    var res = await fetch(getProfileURL, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(reqBody)
-    })
-    var profileJson = await res.json()
-    if (profileJson.success) {
-      return profileJson.profilePicture.profileURL
-    }
-    alert("could not get profile url for id: ", id)
-  }
-
-  async function getUsername(id) {
-    var reqBody = { id }
-    var res = await fetch(getUsernameURL, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(reqBody)
-    })
-    var usernameJson = await res.json()
-    if (usernameJson.success) {
-      return usernameJson.username
-    }
-    alert("could not get profile url for id: ", id)
-  }
-
-module.exports = {
-  getBests, 
-  getProfile, 
-  getUsername
+  alert("could not get bests for id: ", id)
 }
+
+/**
+ * gets the profile pic url of the user 
+ * given their user id in Mongo
+ */
+export async function getProfile(id) {
+  var reqBody = { id }
+  var res = await fetch(getProfileURL, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reqBody)
+  })
+  var profileJson = await res.json()
+  if (profileJson.success) {
+    return profileJson.profilePicture.profileURL
+  }
+  alert("could not get profile url for id: ", id)
+}
+
+export async function getUsername(id) {
+  var reqBody = { id }
+  var res = await fetch(getUsernameURL, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reqBody)
+  })
+  var usernameJson = await res.json()
+  if (usernameJson.success) {
+    return usernameJson.username
+  }
+  alert("could not get profile url for id: ", id)
+}
+
+// module.exports = {
+//   getBests, 
+//   getProfile, 
+//   getUsername,
+// }
